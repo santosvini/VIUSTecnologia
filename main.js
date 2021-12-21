8bb73ae855f4bfb4d47b385fa79ce1cffb405a64
Vini Santos <vini_santos7@live.com>
2021-12-20 21:11:51 -0300

    :construction::memo::hammer: update scripts, the file index, out the folder public, and corrections in styles :construction::memo::hammer:

--- main.js
+++ main.js
@@ -1,4 +1,28 @@
+let name = String(prompt('Qual o seu nome: '))
+  if (name == null) {
+    alert('Queremos te conhecer')
+  }else {
+    alert("Olá, " + name)
+}
+
+var titulo = document.getElementById('h1')
+var cargo = document.getElementById('cargo')
+
+titulo.addEventListener('click', change)
+titulo.addEventListener('mouseenter', enter)
+cargo.addEventListener('click', out)
+cargo.addEventListener('mouseover', out)
+
+
 function change() {
-  var nome = document.getElementById('h1')
-  nome.innerHTML = '<p>Seja Bem-Vindo!</p>'
+  titulo.innerHTML = `Seja Bem-Vindo, ${name}!`
+}
+
+function enter() {
+  titulo.innerHTML = `Você está na ViUs`
+}
+
+function out() {
+  cargo.innerHTML = `Não esqueça de acessar nosso menu,
+   ${name}!`
 }
